ea13ff768a4b4b3ac922c72397cb99dafb2b4b4f
AAANAS <anas.aamoum@gmail.com>
2021-05-18 13:58:24 +0300

    Authorization mixins introduced

--- viewer/views.py
+++ viewer/views.py
@@ -1,6 +1,7 @@
 from django.urls import reverse_lazy
+from django.contrib.auth.models import Group
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView, DetailView
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin, UserPassesTestMixin
 from django.contrib import messages
 from .models import Movie
 from .forms import MovieForm
@@ -9,16 +10,27 @@ from logging import getLogger
 LOGGER = getLogger()
 
 
+class EditorRequiredMixin(UserPassesTestMixin):
+    def test_func(self):
+        editors_group = Group.objects.get(name="Editors")
+        user_groups = self.request.user.groups.all()
+        if editors_group in user_groups:
+            return True
+        else:
+            return False
+        # return editors_group in user_groups
+
 
 class MoviesView(ListView):
     template_name = "viewer/movies.html"
     model = Movie
 
 
-class MovieCreateView(LoginRequiredMixin, CreateView):
+class MovieCreateView(PermissionRequiredMixin, LoginRequiredMixin, CreateView):
     template_name = "form.html"
     form_class = MovieForm
     success_url = reverse_lazy("movie_create")
+    permission_required = "viewer.add_movie"
 
     def form_valid(self, form):
         result = super().form_valid(form)
@@ -32,11 +44,12 @@ class MovieCreateView(LoginRequiredMixin, CreateView):
         return super().form_invalid(form)
 
 
-class MovieUpdateView(LoginRequiredMixin, UpdateView):
+class MovieUpdateView(EditorRequiredMixin, PermissionRequiredMixin, UpdateView):
     template_name = "form.html"
     model = Movie
     form_class = MovieForm
     success_url = reverse_lazy("index")
+    permission_required = "viewer.change_movie"
 
     def form_invalid(self, form):
         messages.warning(self.request, "Invalid form")
@@ -44,12 +57,19 @@ class MovieUpdateView(LoginRequiredMixin, UpdateView):
         return super().form_invalid(form)
 
 
-class MovieDeleteView(LoginRequiredMixin, DeleteView):
+class MovieDeleteView(PermissionRequiredMixin, DeleteView):
     template_name = "viewer/movie_confirm_delete.html"
     model = Movie
     success_url = reverse_lazy("index")
+    permission_required = "viewer.delete_movie"
 
 
 class MovieDetailView(DetailView):
     template_name = "viewer/movie_details.html"
     model = Movie
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['user_groups'] = self.request.user.groups.all()
+        context['user_permissions'] = self.request.user.get_all_permissions()
+        return context

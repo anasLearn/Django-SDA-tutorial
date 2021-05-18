8e7a678e7fef8099c21d8feb215a2d58e82ce438
AAANAS <anas.aamoum@gmail.com>
2021-05-18 17:02:05 +0300

    Admin Panel Customized

--- viewer/admin.py
+++ viewer/admin.py
@@ -1,5 +1,51 @@
 from django.contrib import admin
+from django.contrib.admin import ModelAdmin
 from .models import Genre, Movie
 
 admin.site.register(Genre)
-admin.site.register(Movie)
+
+
+class MovieAdmin(ModelAdmin):
+
+    @staticmethod
+    def released_year(obj):
+        return obj.released.year
+
+    @staticmethod
+    def cleanup_description(modeladmin, request, queryset):
+        queryset.update(description="This description has been cleaned up")
+
+    ordering = ['id']
+    list_display = ['id', 'title', 'genre', 'released_year']
+    list_per_page = 5
+    list_display_links = ['id', 'title']
+    list_editable = ['genre']
+    list_filter = ['genre']
+    search_fields = ['title']
+    actions = ['cleanup_description']
+    date_hierarchy = 'released'
+
+    fieldsets = [
+        (None, {'fields': ['title', 'created']}),
+        (
+            'External Information',
+            {
+                'fields' : ['genre', 'released'],
+                'description': "These fields are containing information about the movie coming from external resources"
+            }
+        ),
+        (
+            'User Information',
+            {
+                'fields': ['rating', 'description'],
+                'description': (
+                    "These fields are coming "
+                    "from the inputs of the website's user"
+                )
+            }
+        )
+    ]
+    readonly_fields = ['created']
+
+
+admin.site.register(Movie, MovieAdmin)
